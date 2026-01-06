e78cf5fa3e083f59ab537c01551060272457c78b
Yash Pandey <yashpandey1907@gmail.com>
2026-01-06 15:30:08 +0530

    feat: implement product search with Meilisearch, add featured products with Redis caching, and retrieve product details by ID.

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getFeaturedProducts, searchProducts } from '../controllers/product.controller.js';
+import { getFeaturedProducts, searchProducts, searchProductById } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ const router = express.Router();
 // URL: /api/products/search?make_id=1&model_id=5&year=2018
 router.get('/search', searchProducts );
 router.get('/featured', getFeaturedProducts);
+router.get('/search/:id', searchProductById );
 
 export default router;
