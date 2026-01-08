c27dfd94969ab5c017f5b67cf8cd8c531f7f5737
raykunal13 <kunalray2701@gmail.com>
2026-01-08 15:26:07 +0530

    fix: Merge Conflixt of Index.js and db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,18 +1,18 @@
-import {Pool} from 'pg';
-import dotenv from 'dotenv';
+import { Pool } from "pg";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-    connectionString: process.env.DB_STRING,
+  connectionString: process.env.DB_STRING,
 });
 
 export async function dbconnect() {
-    try {
-        const client = await pool.connect();
-        const res = await client.query('SELECT version()');
-        console.log("Connected to:", res.rows[0].version); // This will tell you the answer!
-        client.release();
-    } catch (err) {
-        console.error("Database connection error:", err);
-    }
+  try {
+    const client = await pool.connect();
+    const res = await client.query("SELECT version()");
+    console.log("Connected to:", res.rows[0].version); // This will tell you the answer!
+    client.release();
+  } catch (err) {
+    console.error("Database connection error:", err);
+  }
 }
