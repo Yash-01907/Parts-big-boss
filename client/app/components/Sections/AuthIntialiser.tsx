58cd1dec43b48f72eb53567919ccfffd7d258c75
Pandey Yash Rakesh <yashpandey1907@gmail.com>
2026-02-13 18:56:34 +0530

    Fixed the infinite log in page loop issue

--- client/app/components/Sections/AuthIntialiser.tsx
+++ client/app/components/Sections/AuthIntialiser.tsx
@@ -3,12 +3,24 @@
 import { useEffect } from "react";
 import { bootstrapAuth } from "../../store/bootstrapAuth";
 import { cartStore } from "../../store/useCartStore";
+import { useAuthStore } from "../../store/useAuthStore";
 
 export default function AuthInitializer() {
+  const { isAuthenticated } = useAuthStore();
+
+  // 1. Bootstrap auth on mount
   useEffect(() => {
     bootstrapAuth();
-    cartStore.fetchCart();
   }, []);
 
-  return null; // This component renders nothing, just runs logic
+  // 2. Sync cart with auth state
+  useEffect(() => {
+    if (isAuthenticated) {
+      cartStore.fetchCart();
+    } else {
+      cartStore.clearCart();
+    }
+  }, [isAuthenticated]);
+
+  return null;
 }
